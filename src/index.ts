d6679fa575a1286b897b142702729876a6695d76
Adrià Navarrete <adri.navarrete.09@gmail.com>
2023-09-25 22:55:41 +0200

    Add request commandBus and queryBus custom attributes in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Application, Request, Response } from 'express'
+import { createContainer } from './backend/shared/container_factory'
+import { CommandQuery } from './backend/shared/buses/command_query'
+
+declare module 'express-serve-static-core' {
+	interface Request {
+		commandBus?: any
+		queryBus?: any
+	}
+}
 
 const app: Application = express()
-const port: string | Number = process.env.PORT || 3000
+const port: string | Number = process.env.PORT || 3001
 
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
@@ -9,7 +18,18 @@ app.listen(port, () => {
 
 app.use(express.json())
 
-// app.use to handle command, query and event
+const { queryBus, commandBus } = createContainer()
+
+app.use((req, res, next) => {
+	req.commandBus = {
+		handle: (command: CommandQuery) => commandBus.handle(command),
+	}
+	req.queryBus = {
+		handle: (query: CommandQuery) => queryBus.handle(query),
+	}
+
+	return next()
+})
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello! I am developed with TS')
